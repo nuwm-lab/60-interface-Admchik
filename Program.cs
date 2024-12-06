1c0f14e6bb5cb0678f124ac6dc3a667899dfdc85
Admchik <127297619+Admchik@users.noreply.github.com>
2024-12-07 00:50:35 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,123 +8,67 @@ namespace LabWork
     // Відео-інструкції щодо роботи з github можна переглянути 
     // за посиланням https://www.youtube.com/@ViktorZhukovskyy/videos 
 
-   
-        // Базовий клас
-        class Human
+
+    // Базовий клас
+    internal class Human
+    {
+        public string FirstName;
+
+        public string LastName;
+
+        public string MiddleName;
+
+        public int BirthDay;
+
+        public int BirthMonth;
+
+        public int BirthYear;
+
+        public virtual void InputData()
         {
-            public string FirstName;
-            public string LastName;
-            public string MiddleName;
-            public int BirthDay;
-            public int BirthMonth;
-            public int BirthYear;
-
-            // Віртуальний метод для введення даних
-            public virtual void InputData()
-            {
-                Console.Write("Введіть ім'я: ");
-                FirstName = Console.ReadLine();
-
-                Console.Write("Введіть прізвище: ");
-                LastName = Console.ReadLine();
-
-                Console.Write("Введіть по-батькові: ");
-                MiddleName = Console.ReadLine();
-
-                Console.Write("Введіть день народження: ");
-                BirthDay = Convert.ToInt32(Console.ReadLine());
-
-                Console.Write("Введіть місяць народження: ");
-                BirthMonth = Convert.ToInt32(Console.ReadLine());
-
-                Console.Write("Введіть рік народження: ");
-                BirthYear = Convert.ToInt32(Console.ReadLine());
-            }
-
-            // Віртуальний метод для обчислення віку
-            public virtual void CalculateAge()
-            {
-                DateTime currentDate = DateTime.Now;
-                int age = currentDate.Year - BirthYear;
-
-                if (currentDate.Month < BirthMonth ||
-                    (currentDate.Month == BirthMonth && currentDate.Day < BirthDay))
-                {
-                    age--;
-                }
-
-                Console.WriteLine($"Вік людини: {age} років");
-            }
-
-            // Віртуальний метод для підрахунку літер
-            public virtual void CountLetterOccurrences()
-            {
-                Console.Write("Введіть літеру для пошуку в прізвищі: ");
-                char letter = Console.ReadKey().KeyChar;
-                Console.WriteLine();
-
-                int count = 0;
-                foreach (char c in LastName)
-                {
-                    if (char.ToLower(c) == char.ToLower(letter))
-                    {
-                        count++;
-                    }
-                }
-
-                Console.WriteLine($"Кількість входжень літери '{letter}' у прізвищі: {count}");
-            }
+            Console.Write("Введіть ім'я: ");
+            FirstName = Console.ReadLine();
+            Console.Write("Введіть прізвище: ");
+            LastName = Console.ReadLine();
+            Console.Write("Введіть по-батькові: ");
+            MiddleName = Console.ReadLine();
+            Console.Write("Введіть день народження: ");
+            BirthDay = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введіть місяць народження: ");
+            BirthMonth = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введіть рік народження: ");
+            BirthYear = Convert.ToInt32(Console.ReadLine());
         }
 
-        // Похідний клас
-        class ExtendedHuman : Human
+        public virtual void CalculateAge()
         {
-            public string FavoriteHobby;
-
-            // Перевизначення методу для введення даних
-            public override void InputData()
+            DateTime currentDate = DateTime.Now;
+            int age = currentDate.Year - BirthYear;
+            if (currentDate.Month < BirthMonth || (currentDate.Month == BirthMonth && currentDate.Day < BirthDay))
             {
-                base.InputData();
-
-                Console.Write("Введіть улюблене хобі: ");
-                FavoriteHobby = Console.ReadLine();
+                age--;
             }
-
-            // Перевизначення виводу додаткової інформації
-            public override void CountLetterOccurrences()
-            {
-                base.CountLetterOccurrences();
-                Console.WriteLine($"Улюблене хобі: {FavoriteHobby}");
-            }
+            Console.WriteLine($"Вік людини: {age} років");
         }
 
-        class Program
+        public virtual void CountLetterOccurrences()
         {
-            static void Main(string[] args)
+            Console.Write("Введіть літеру для пошуку в прізвищі: ");
+            char letter = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+            int count = 0;
+            string lastName = LastName;
+            foreach (char c in lastName)
             {
-                Console.WriteLine("Виберіть режим: 1 - Базова людина, 2 - Розширена людина");
-                char userChoice = Console.ReadKey().KeyChar;
-                Console.WriteLine();
-
-                // Динамічне створення об'єкту
-                Human person;
-                if (userChoice == '1')
+                if (char.ToLower(c) == char.ToLower(letter))
                 {
-                    person = new Human();
+                    count++;
                 }
-                else
-                {
-                    person = new ExtendedHuman();
-                }
-
-                // Використання віртуальних методів
-                person.InputData();
-                person.CalculateAge();
-                person.CountLetterOccurrences();
-
-                Console.WriteLine("\nПрограму завершено.");
             }
+            Console.WriteLine($"Кількість входжень літери '{letter}' у прізвищі: {count}");
         }
-    
+    }
+
+
 
 }
